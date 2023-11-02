a6718482d39c8366444ed117ee68a538c1150d35
Christian Naykene <younchriss@live.co.uk>
2023-11-02 22:23:41 +0000

    add input validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,26 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.destination ||
+    !launch.launchDate
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    //isNaN check if launchDate.valueOf() which returns a number if date is valid
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
