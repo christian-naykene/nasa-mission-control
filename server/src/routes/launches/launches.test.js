0a6cf5f8869b564e4c7e26756cf7294fc3dbce0f
Christian Naykene <younchriss@live.co.uk>
2023-11-08 22:55:56 +0000

    add unit tests

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,63 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  const launchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
+
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(launchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(requestDate).toBe(responseDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It should respond 400 missing launch property", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toEqual({
+      error: "Missing required launch property",
+    });
+  });
+
+  test("It should catch invalid launch date", async () => {
+    launchDataWithoutDate.launchDate = "not a date";
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toEqual({
+      error: "Invalid launch date",
+    });
+  });
+});
